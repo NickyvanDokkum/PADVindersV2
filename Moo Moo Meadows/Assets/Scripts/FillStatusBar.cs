4f268ac8486e7773175cb6529be1a7c8adc26100
J. Riedeman <j.riedeman@hva.nl>
2020-05-07 14:22:30 +0200

    initial push

---
+++ Moo Moo Meadows/Assets/Scripts/FillStatusBar.cs
@@ -0,0 +1,53 @@
+﻿       using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FillStatusBar: MonoBehaviour
+{
+    public float currentStats;
+    public float maxStats;
+    public Image fillimage;
+    private Slider slider;
+
+    private bool started = false;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+
+        slider = GetComponent<Slider>();
+        Debug.Log("slider start" + slider);
+        ChangeValue();
+    }
+
+    //Deze funtie wordt aangeroepen als de stats veranderen en zorgt ervoor dat de verandering in de slider komt
+    void ChangeValue()
+    {
+        if (!started)
+        {
+            started = !started;
+            Start();
+        }
+
+        float fillValue = currentStats / maxStats;
+        Debug.Log("slider" + slider);
+        Debug.Log("slider value" + slider.value);
+        slider.value = fillValue; 
+    }
+
+    //Functie wordt gebruikt om de stats te veranderen
+    public void SetStat(int stat)
+    {
+        currentStats = stat;
+        ChangeValue();
+    }
+
+    //Functie wordt gebruikt om de stats te vergroten
+    public void AddValue(int stat)
+    {
+        currentStats += stat;
+        ChangeValue();
+    }
+    
+}

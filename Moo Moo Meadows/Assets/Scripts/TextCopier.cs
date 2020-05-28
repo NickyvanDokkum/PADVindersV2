850270a206cabe645f55e19b2ea87eea12d8e195
Nicky van Dokkum <nickvandokkum@hotmail.nl>
2020-05-28 14:04:57 +0200

    fixed the shop screen

---
+++ Moo Moo Meadows/Assets/Scripts/TextCopier.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextCopier : MonoBehaviour
+{
+    [SerializeField] private Text thisText;
+    [SerializeField] private Text textToCopy;
+    void Update()
+    {
+        if(thisText.text != textToCopy.text) {
+            thisText.text = textToCopy.text;
+        }
+    }
+}
